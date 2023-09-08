3a7cacfd2546342a17bdf76411a79475cee44424
Mau03gm <gatojs0109@outlook.com>
2023-09-08 09:37:55 -0600

    work experience

--- index.js
+++ index.js
@@ -9,14 +9,39 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get("/ruta-prueba", (req,res)=>{
+app.get("/ruta-prueba", (req, res) => {
+    const contacto = {
+      "x": "mau03",
+      "instagram": "mau03._",
+      "numero": "2712098996"
+    };
+    const work_experience = [
+        {
+          "puesto": "Desarrollador en Spring Boot",
+          "descripcion": "Trabajé en un proyecto de desarrollo de una aplicación web utilizando el framework Spring Boot."
+        },
+        {
+          "puesto": "Diseñador UX/UI",
+          "descripcion": "Diseñé la interfaz de usuario y la experiencia de usuario para aplicaciones móviles y web."
+        },
+        {
+          "puesto": "Analista de Datos",
+          "descripcion": "Realicé análisis de datos para informar las decisiones empresariales y optimizar las operaciones."
+        },
+        {
+          "puesto": "Gerente de Proyectos",
+          "descripcion": "Gestioné proyectos de desarrollo de software de principio a fin, asegurando la entrega a tiempo y dentro del presupuesto."
+        }
+      ];
     return res.status(200).json({
-        "hola": "jeje hola",
-        
-    })
-})
+        contacto: contacto,
+        work_experience: work_experience
+      });
+    });
+  
 
 //poner servidor a escuchar http
 app.listen(port, ()=>{
     console.log("servidor node on port: ", port)
 })
+
